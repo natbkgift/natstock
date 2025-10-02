9dccdaccd854713331e4d4fbd0fe2f5a90b52f55
natbkgift <40134401+natbkgift@users.noreply.github.com>
2025-10-02 20:09:38 +0700

    Ensure test app key and env bootstrap

--- inventory-app/tests/TestCase.php
+++ inventory-app/tests/TestCase.php
@@ -6,5 +6,15 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        config(['app.key' => env('APP_KEY', $this->testingAppKey())]);
+    }
+
+    protected function testingAppKey(): string
+    {
+        return 'base64:TPsL1tiSnUiGKe11FbyhTSMp+04o291B9Xj78a39qQs=';
+    }
 }
